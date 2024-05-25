89f7e345dcb31de66f6dbe2a11bd0d0b6f9fe3e7
jesivaldi <arqjesicavaldivia@gmail.com>
2024-05-24 21:37:50 -0300

    cambio nombre

---
+++ 4.1 ej.2 estr secuenciales.py
@@ -0,0 +1,11 @@
+# Un pintor de casas debe hacer un presupuesto para un cliente. Lo
+
+#que cobra se calcula de acuerdo a los metros cuadrados que debe pintar. El
+
+#cliente le indica que necesita conocer el costo de mano de obra para pintar una
+
+#pared rectangular de un galpón. El pintor cobra un monto ﬁjo por cada metro
+
+#cuadrado. Puedes hacer un algoritmo para calcular el costo de mano de obra para
+
+#pintar la pared.
